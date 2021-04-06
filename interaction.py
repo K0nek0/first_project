405475d6e1cd0ca62e6b5646757f9e5809fe6c3c
K0nek0 <>
2021-04-06 16:54:55 +0000

    update

--- interaction.py
+++ interaction.py
@@ -1,1 +1,39 @@
-print()
+import numpy as np
+from scipy.integrate import odeint
+
+G = 6.67 * 10**(-11)
+ae = 149.6 * 10**9
+m_c = 1.9885 * 10**30
+x_c = 0
+y_c = 0
+
+T = 365
+t_end = 24 * 3600 * 365
+t = np.linspace(0, t_end, T)
+
+class Interaction_small:
+    def __init__(self, x0, vx0, y0, vy0):
+        self.x0 = x0
+        self.vx0 = vx0
+        self.y0 = y0
+        self.vy0 = vy0
+
+    def _func(self, s, t):
+        x, vx, y, vy = s
+
+        dxdt = vx
+        dvxdt = (-G * m_c * (x - x_c) / ((x - x_c)**2 + (y - y_c)**2)**1.5)
+
+        dydt = vy
+        dvydt = (-G * m_c * (y - y_c) / ((x - x_c)**2 + (y - y_c)**2)**1.5)
+
+        return dxdt, dvxdt, dydt, dvydt
+
+    def solve_func(self):
+        _s0 = self.x0, self.vx0, self.y0, self.vy0
+        sol = odeint(self._func, _s0, t)
+
+        self.x = sol[:, 0]
+        self.y = sol[:, 2]
+
+        return self.x / ae, self.y / ae
--- main.py
+++ main.py
@@ -1,1 +1,85 @@
-print('')
+import interaction as i
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import Ellipse, Color
+from kivy.vector import Vector
+from kivy.clock import Clock
+from kivy.uix.button import Button
+
+
+class Object(Widget):
+    def __init__(self, **kw):
+        super(Object, self).__init__()
+
+        self.step = kw['step']
+        self.color = kw['color']
+        self.pos = kw['pos']
+        self.vel = kw['vel']
+        self.size = kw['size']
+        self.COORDS_small = []
+
+        self.interect_small = i.Interaction_small(x0=self.pos[0],
+                                                  vx0=self.vel[0],
+                                                  y0=self.pos[1],
+                                                  vy0=self.vel[1])
+
+        for j in range(self.step):
+            self.COORDS_small.append(
+                (float(self.interect_small.solve_func()[0][j]) * 100 + 250,
+                 float(self.interect_small.solve_func()[1][j]) * 100 + 250))
+
+
+    def draw_small(self):
+        self.canvas.add(self.color)
+        self.ellipse = Ellipse(pos=self.pos, size=self.size)
+        self.ellipse_c = Ellipse(pos=(250, 250), size=(20, 20))
+        self.canvas.add(self.ellipse)
+        self.canvas.add(self.ellipse_c)
+
+    def move_small(self, k):
+        self.pos = Vector(self.COORDS_small[k])
+        self.ellipse.pos = self.pos
+
+
+class Move(Widget):
+    def __init__(self):
+        super(Move, self).__init__()
+        self.counter = 0
+        self.n = i.T
+
+    def update_small(self, dt):
+        if self.counter == self.n - 1:
+            self.counter = 0
+        self.counter += 1
+        self.object.move_small(self.counter)
+
+    def create_small(self, color, pos, size, vel):
+        self.object = Object(step=self.n,
+                             color=color,
+                             pos=pos,
+                             vel=vel,
+                             size=size)
+        self.object.draw_small()
+        self.add_widget(self.object)
+
+
+class PlanetApp(App):
+    def create_small_object(self, instance):
+        self.object = Move()
+        self.object.create_small(color=Color(1, 0, 0),
+                                 pos=(0, 1 * 149 * 10**9),
+                                 vel=(-30000, 0),
+                                 size=(8, 8))
+        Clock.schedule_interval(self.object.update_small, .04)
+        self.parent.add_widget(self.object)
+
+    def build(self):
+        self.parent = Widget()
+        self.btn_1 = Button(text='Create small object',
+                            on_press=self.create_small_object)
+        self.parent.add_widget(self.btn_1)
+
+        return self.parent
+
+
+PlanetApp().run()
