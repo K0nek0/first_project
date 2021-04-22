7dbce7938d4909065807b2a5563c203108d8f967
K0nek0 <>
2021-04-22 17:17:24 +0000

    работает

--- main.py
+++ main.py
@@ -40,6 +40,59 @@ class Object(Widget):
         self.pos = Vector(self.COORDS_small[k])
         self.ellipse.pos = self.pos
 
+class PainterWidget(Widget):
+    def on_touch_down(self, touch):
+        with self.canvas:
+            # Color(1,0,0,1)
+            # self.ellipse = Ellipse(pos=(touch.x, touch.y), size = (10,10))
+
+            self.x1 = touch.x
+            self.y1 = touch.y
+            print('x1_self: ',self.x1)
+            print('y1_self: ',self.y1)
+
+    def on_touch_up(self, touch):
+
+            self.k=200
+            self.x2 = touch.x
+            self.y2 = touch.y
+            # Color(0,0,0,1)
+            # self.ellipse = Ellipse(pos=(self.x1, self.y1), size = (10,10))
+
+            # x и y векторы 
+            self.x_vector = self.x1 - self.x2
+            self.y_vector = self.y1 - self.y2
+            print('x2_self: ',self.x2)
+            print('y2_self: ',self.y2)
+
+            # модуль
+            self.vector_modul = (self.x_vector**2+self.y_vector**2)**(1/2)
+
+            try:
+                self.cos_phi = self.x_vector/self.vector_modul
+            except ZeroDivisionError:
+                self.cos_phi = 0
+
+            try:
+                self.sin_phi = self.y_vector/self.vector_modul
+            except ZeroDivisionError:
+                self.sin_phi = 0
+
+
+            self.v_modul = self.k*self.vector_modul
+            # скорости
+            self.vx = self.v_modul*self.cos_phi
+            self.vy = self.v_modul*self.sin_phi
+            print('vx: ',self.vx)
+            print('vy: ',self.vy)
+            self.object = Move()
+            self.object.create_small(color=Color(1, 0, 0),
+                                    pos=(self.x1* 149 * 10**6, self.y1* 149 * 10**7),
+                                    vel=(self.vx, self.vy),
+                                    size=(8, 8))
+            Clock.schedule_interval(self.object.update_small, .04)
+            self.parent.add_widget(self.object)
+
 
 class Move(Widget):
     def __init__(self):
@@ -64,22 +117,9 @@ class Move(Widget):
 
 
 class PlanetApp(App):
-    def create_small_object(self, instance):
-        self.object = Move()
-        self.object.create_small(color=Color(1, 0, 0),
-                                 pos=(0, 1 * 149 * 10**9),
-                                 vel=(-30000, 0),
-                                 size=(8, 8))
-        Clock.schedule_interval(self.object.update_small, .04)
-        self.parent.add_widget(self.object)
 
     def build(self):
-        self.parent = Widget()
-        self.btn_1 = Button(text='Create small object',
-                            on_press=self.create_small_object)
-        self.parent.add_widget(self.btn_1)
-
-        return self.parent
+        return PainterWidget()
 
 
 PlanetApp().run()
